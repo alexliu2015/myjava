be6dd8dc098365a92d70fa1d0b5d09f92bf22142
alexliu2015 <liuyi19890105@gmail.com>
2017-09-12 06:57:16 -0700

    add all java file into git

---
+++ bittest.java
@@ -0,0 +1,8 @@
+public class bittest {
+  public static void main(String[] args) {
+      int a = -3;
+      int b = 2;
+      System.out.println(Integer.toBinaryString(a));
+      System.out.println(Integer.toBinaryString(b));
+  }
+}
