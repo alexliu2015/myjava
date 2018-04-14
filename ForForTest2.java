778a22c424889e02f6e0394b836b737f662e9234
alexliu2015 <liuyi19890105@gmail.com>
2018-04-14 01:20:46 -0700

    modify forfortest2 file to adjust bug fix

--- ForForTest2.java
+++ ForForTest2.java
@@ -3,7 +3,7 @@ import java.util.*;
 class ForFOrTest2{
     public static void main(String[] args){
         for(int i=1;i<=5;i++){
-            for(int j=1;j<=i;j++){
+            for(int j=1;j<i;j++){
                 System.out.print(" ");
             }
             for(int y=i;y<=5;y++){
