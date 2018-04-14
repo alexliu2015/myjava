8ff87ce872572bbcdfcd6ca61cc469c03e027f36
alexliu2015 <liuyi19890105@gmail.com>
2018-04-14 01:16:46 -0700

    add ForForTest to test for loop in java

---
+++ ForForTest2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class ForFOrTest2{
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int y=i;y<=5;y++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
